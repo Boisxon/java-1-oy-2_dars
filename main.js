37324151d02392e9188c83aca39ed79aa31c4d90
Boisxon <boisxon357@gmail.com>
2024-12-07 19:59:17 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var ism = prompt(" String typedagi malumot kiriting ")
+console.log(`Siz kiritgan malumot  ${  typeof ism}`);
+
+var bot = +prompt("Number typedagi malumot kiriting ")
+console.log(typeof Number(bot));
+
+var son = prompt("Boolean typedagi malumot kiriting ")
+console.log(typeof Boolean(son));
+
+confirm(" Tori malumot qoydismi? ") 
